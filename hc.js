e873d4f509c4cb2600f50f143a9e0aaf8e1e793c
tcv <mikew@thecomputervalet.com>
2013-08-12 20:38:17 -0500

    updated javascript

--- hc.js
+++ hc.js
@@ -1,8 +1,10 @@
 var answer = "5";
 function game() {
 	var guess = prompt("Choose a number!");
-	if (guess !== answer)
+	if (guess !== answer) {
+        console.log("Nope!");
         game();
+	}
 	else console.log("Yay!");
 }
 game();
