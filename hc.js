8c8f8cb37f6b7d351f30d38d17ec70b80c6e75aa
tcv <mikew@thecomputervalet.com>
2013-08-15 17:12:50 -0500

    new css file and jscript additions

--- hc.js
+++ hc.js
@@ -1,17 +1,20 @@
-function getSecret (min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var answer = Math.floor(Math.random() * 10) +1;
-console.log(answer);
-function game() {
-	var guess = prompt("Choose a number!");
-	guess = Number(guess);
-	if (guess !== answer) {
-        console.log("Nope!");
-        game();
-    }
-    else {
-	console.log("Yay!");
+$(document).ready(function() {
+	function getSecret (min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
-}
-game();
+	var answer = Math.floor(Math.random() * 10) +1;
+	console.log(answer);
+	function game() {
+		var guess = prompt("Choose a number!");
+		guess = Number(guess);
+		if (guess !== answer) {
+	        console.log("Nope!");
+	        $('#bg').animate(document.getElementById('bg').style.backgroundColor = "444444", 1000);
+	        game();
+	    }
+	    else {
+		console.log("Yay!");
+		}
+	}
+	game();
+});
