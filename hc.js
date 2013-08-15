04abe072b681480be4d9ab5fcbf8111207deb802
tcv <mikew@thecomputervalet.com>
2013-08-14 20:32:52 -0500

    random code and better if else

--- hc.js
+++ hc.js
@@ -1,10 +1,17 @@
-var answer = "5";
+function getSecret (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var answer = Math.floor(Math.random() * 10) +1;
+console.log(answer);
 function game() {
 	var guess = prompt("Choose a number!");
-	while (guess !== answer) {
+	guess = Number(guess);
+	if (guess !== answer) {
         console.log("Nope!");
         game();
-	}
+    }
+    else {
 	console.log("Yay!");
+	}
 }
 game();
