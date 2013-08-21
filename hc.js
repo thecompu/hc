d760552673bdf1584da2481ca4012c1ef3d4e784
tcv <mikew@thecomputervalet.com>
2013-08-21 15:51:45 -0500

    ugh

--- hc.js
+++ hc.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
 	var answer = Math.floor(Math.random() * 10) +1;
 	console.log(answer);
-	function game() {
-		var guess = prompt("Choose a number!");
+	$("#game").on("submit", function(){
+	var guess = $("#guess").val();
+		if (guess !== answer) {
+	        $("#bg").animate({
+	        	backgroundColor: "#F00",
+	        	color: "#000",
+	        }, 1000 );
+	        console.log("Nope!");
+	    }
+	    else {
+		console.log("Yay!");
+		}
+	});
+});
+
+/*	function game() {
 		guess = Number(guess);
 		if (guess !== answer) {
 	        $("#bg").animate({
@@ -17,4 +31,4 @@ $(document).ready(function() {
 		}
 	}
 	game();
-});
+});*/
