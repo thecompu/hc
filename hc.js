09f1e0cc0e4135a9c491a4ced2fc78bf9c89451a
tcv <mikew@thecomputervalet.com>
2013-08-23 17:51:51 -0500

    yay colors!

--- hc.js
+++ hc.js
@@ -2,33 +2,23 @@ $(document).ready(function() {
 	var answer = Math.floor(Math.random() * 10) +1;
 	console.log(answer);
 	$("#game").on("submit", function(){
-	var guess = $("#guess").val();
-		if (guess !== answer) {
-	        $("#bg").animate({
-	        	backgroundColor: "#F00",
-	        	color: "#000",
-	        }, 1000 );
-	        console.log("Nope!");
-	    }
-	    else {
-		console.log("Yay!");
-		}
-	});
+		event.preventDefault();
+		var guess = $("#guess").val();
+			console.log("Heading to Check!")
+			if (guess == answer) {
+				$("#bg").animate({
+					backgroundColor: "#F3FF00"
+				})
+			}
+			else if (Math.abs(answer - guess) <= 3) {
+				console.log("Within 3 block")
+		        $("#bg").animate({
+		        	backgroundColor: "#FF0000"
+		        }, 1000 );
+		        console.log("Nope!");
+		    }
+		    else {
+			console.log("Ending now!");
+			}
+		});
 });
-
-/*	function game() {
-		guess = Number(guess);
-		if (guess !== answer) {
-	        $("#bg").animate({
-	        	backgroundColor: "#F00",
-	        	color: "#000",
-	        }, 1000 );
-	        console.log("Nope!");
-	        game();
-	    }
-	    else {
-		console.log("Yay!");
-		}
-	}
-	game();
-});*/
